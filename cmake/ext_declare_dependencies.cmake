b566a81619c807f0d57c36aadcced96527040181
Jan Christoph Uhde <Jan@UhdeJC.com>
2023-03-17 20:38:09 +0100

    fix: dependencies handling

--- cmake/ext_declare_dependencies.cmake
+++ cmake/ext_declare_dependencies.cmake
@@ -1,48 +1,47 @@
-if(NOT EXT_DOWNLOAD)
-    return()
-endif()
+macro(EXT_DECLARE_DEPENDENCIES)
+    ext_log("declaring FetchContent dependencies")
+    include(FetchContent)
+    if(EXT_DOWNLOAD)
+        # our libs
+        FetchContent_Declare(
+            ExtBasics
+            GIT_REPOSITORY https://github.com/extcpp/basics
+            GIT_TAG        master
+            GIT_PROGRESS   TRUE
+            OVERRIDE_FIND_PACKAGE
+        )
 
-ext_log("declaring FetchContent dependencies")
-include(FetchContent)
+        FetchContent_Declare(
+            Args
+            GIT_REPOSITORY https://github.com/obiwahn/args
+            GIT_TAG        master
+            GIT_PROGRESS   TRUE
+            OVERRIDE_FIND_PACKAGE
+        )
 
-# our libs
-FetchContent_Declare(
-    ext-basics
-    GIT_REPOSITORY https://github.com/extcpp/basics
-    GIT_TAG        master
-    GIT_PROGRESS   TRUE
-    OVERRIDE_FIND_PACKAGE
-)
+        # ohter libs
+        FetchContent_Declare(
+            googletest
+            GIT_REPOSITORY https://github.com/google/googletest.git
+            GIT_TAG        58d77fa8070e8cec2dc1ed015d66b454c8d78850 # v1.12.1
+            GIT_PROGRESS   TRUE
+            OVERRIDE_FIND_PACKAGE
+        )
 
-FetchContent_Declare(
-    args
-    GIT_REPOSITORY https://github.com/obiwahn/args
-    GIT_TAG        master
-    GIT_PROGRESS   TRUE
-    OVERRIDE_FIND_PACKAGE
-)
+        FetchContent_Declare(
+            cryptopp
+            GIT_REPOSITORY https://github.com/abdes/cryptopp-cmake
+            GIT_TAG        13cb0fe7e76410258b76975e77fd407400e3f2d1
+            GIT_PROGRESS   TRUE
+            OVERRIDE_FIND_PACKAGE
+        )
 
-# ohter libs
-FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG        58d77fa8070e8cec2dc1ed015d66b454c8d78850 # v1.12.1
-    GIT_PROGRESS   TRUE
-    OVERRIDE_FIND_PACKAGE
-)
-
-FetchContent_Declare(
-    cryptopp
-    GIT_REPOSITORY https://github.com/abdes/cryptopp-cmake
-    GIT_TAG        13cb0fe7e76410258b76975e77fd407400e3f2d1
-    GIT_PROGRESS   TRUE
-    OVERRIDE_FIND_PACKAGE
-)
-
-FetchContent_Declare(
-    cpp-subprocess
-    GIT_REPOSITORY https://github.com/arun11299/cpp-subprocess
-    GIT_TAG        1392c47cbbf519bd02ddafb5871526c16b7f2dd1
-    GIT_PROGRESS   TRUE
-    OVERRIDE_FIND_PACKAGE
-)
+        FetchContent_Declare(
+            Subprocess
+            GIT_REPOSITORY https://github.com/arun11299/cpp-subprocess
+            GIT_TAG        1392c47cbbf519bd02ddafb5871526c16b7f2dd1
+            GIT_PROGRESS   TRUE
+            OVERRIDE_FIND_PACKAGE
+        )
+    endif(EXT_DOWNLOAD)
+endmacro(EXT_DECLARE_DEPENDENCIES)
