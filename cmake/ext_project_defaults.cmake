9c09b50b16e595814fa74713f58cec69f1b38aed
Jan Christoph Uhde <Jan@UhdeJC.com>
2023-03-17 23:03:28 +0100

    fix: file and function for colored output match now

--- cmake/ext_project_defaults.cmake
+++ cmake/ext_project_defaults.cmake
@@ -6,7 +6,7 @@
 macro(EXT_PROJECT_DEFAULTS)
     set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
     set_property(GLOBAL PROPERTY USE_FOLDERS ON) # XCode / VS folders
-    ext_colored_compiler_ouput(ON)
+    EXT_COLORED_OUTPUT(ON)
     ext_define_warning_flags()
 
     EXT_DEFAULT_INSTALL_PREFIX()
