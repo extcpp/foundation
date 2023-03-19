6a51036d55bdcba3e0b0cdad2f389b706a7bc711
Jan Christoph Uhde <Jan@UhdeJC.com>
2023-03-19 21:26:41 +0100

    add: cmake decalatations for abseil

--- cmake/ext_declare_dependencies.cmake
+++ cmake/ext_declare_dependencies.cmake
@@ -28,17 +28,17 @@ macro(EXT_DECLARE_DEPENDENCIES)
 
         # ohter well known libs
         FetchContent_Declare(
-            Folly
-            GIT_REPOSITORY https://github.com/facebook/folly.git #v2023.03.13.00
-            GIT_TAG        ce2b95715de229fcb51bd97410469a3ad4d2bfb2
+            GTest
+            GIT_REPOSITORY https://github.com/google/googletest.git
+            GIT_TAG        58d77fa8070e8cec2dc1ed015d66b454c8d78850 # v1.12.1
             GIT_PROGRESS   TRUE
             OVERRIDE_FIND_PACKAGE
         )
 
         FetchContent_Declare(
-            googletest
-            GIT_REPOSITORY https://github.com/google/googletest.git
-            GIT_TAG        58d77fa8070e8cec2dc1ed015d66b454c8d78850 # v1.12.1
+            Abseil
+            GIT_REPOSITORY https://github.com/abseil/abseil-cpp.git  #20230125.1
+            GIT_TAG        c8a2f92586fe9b4e1aff049108f5db8064924d8e
             GIT_PROGRESS   TRUE
             OVERRIDE_FIND_PACKAGE
         )
@@ -50,5 +50,21 @@ macro(EXT_DECLARE_DEPENDENCIES)
             GIT_PROGRESS   TRUE
             OVERRIDE_FIND_PACKAGE
         )
+
+        # FetchContent_Declare(
+        #     Glog
+        #     GIT_REPOSITORY https://github.com/google/glog.git # v0.6.0
+        #     GIT_TAG        b33e3bad4c46c8a6345525fd822af355e5ef9446
+        #     GIT_PROGRESS   TRUE
+        #     OVERRIDE_FIND_PACKAGE
+        # )
+
+        # FetchContent_Declare(
+        #     Folly #required glog
+        #     GIT_REPOSITORY https://github.com/facebook/folly.git #v2023.03.13.00
+        #     GIT_TAG        ce2b95715de229fcb51bd97410469a3ad4d2bfb2
+        #     GIT_PROGRESS   TRUE
+        #     OVERRIDE_FIND_PACKAGE
+        # )
     endif(EXT_DOWNLOAD)
 endmacro(EXT_DECLARE_DEPENDENCIES)
