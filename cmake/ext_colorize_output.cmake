3728a69b170396e457a313e40d63e6b61c7e9f5e
Jan Christoph Uhde <Jan@UhdeJC.com>
2023-09-28 23:36:14 +0200

    fix: use colors always

--- cmake/ext_colorize_output.cmake
+++ cmake/ext_colorize_output.cmake
@@ -5,7 +5,7 @@
 # -----------------------------------------------------------------------------
 macro(EXT_COLORIZE_OUTPUT)
     if(NOT DEFINED EXT_COLORIZE_OUTPUT_ENABLED)
-        set(EXT_COLORIZE_OUTPUT_ENABLED TRUE CACHE BOOL "Use colored compiler output.")
+        set(EXT_COLORIZE_OUTPUT_ENABLED ON)
         if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
             add_compile_options(-fdiagnostics-color=always)
         elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
